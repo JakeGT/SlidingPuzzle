13abb507fa22e321555d3b93708051a62d062fae
JakeGT <jaketalling@rocketmail.com>
2023-02-02 12:53:06 +0000

    Added documentation.

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -6,7 +6,16 @@ import java.util.Objects;
 public class BoardGUI {
     private final JFrame frame = new JFrame("Board");
     private final PuzzleBoard board;
+
     private final ArrayList<ArrayList<JButton>> jButtons = new ArrayList<>();
+    // jButtons will organise buttons into a 2D array of (row, column)
+
+    /**
+     * BoardGUI Constructor
+     * Just some boilerplate code for Swing.
+     *
+     * @param board The PuzzleBoard object
+     */
     BoardGUI(PuzzleBoard board) {
         this.board = board;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,6 +25,12 @@ public class BoardGUI {
         createTiles();
         frame.setVisible(true);
     }
+
+    /**
+     * This is used to create the JButtons that act as tiles.
+     * Each button represents the tiles in the puzzle game. Each button will be given an action listener to the move
+     * piece method.
+     */
 
     private void createTiles() {
         for (int y = 0; y<4; y++) {
@@ -36,22 +51,37 @@ public class BoardGUI {
         }
     }
 
+
+    /**
+     * This takes the position of the respective button and uses this to call the PuzzleBoard object method.
+     *
+     * @param x X-coord of the piece to move
+     * @param y Y-coord of the piece to move
+     */
     private void movePiece(int x, int y) {
         try {
             if (board.movePiece(x, y)) {
-                for(int a = 0; a < 4; a++) {
-                    for(int b = 0; b < 4; b++) {
-                        String tile = board.getTile(b, a);
-                        if (!Objects.equals(tile, "0")) {
-                            jButtons.get(a).get(b).setText(tile);
-                        } else {
-                            jButtons.get(a).get(b).setText("");
-                        }
-                    }
-                }
+                updateBoardGUI();
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Updates the boardGUI to match the PuzzleBoard.
+     */
+    private void updateBoardGUI() {
+        for(int a = 0; a < 4; a++) {
+            for(int b = 0; b < 4; b++) {
+                String tile = board.getTile(b, a);
+                if (!Objects.equals(tile, "0")) {
+                    jButtons.get(a).get(b).setText(tile);
+                } else {
+                    jButtons.get(a).get(b).setText("");
+                }
+            }
         }
     }
 }
--- src/PuzzleBoard.java
+++ src/PuzzleBoard.java
@@ -9,9 +9,14 @@ public class PuzzleBoard {
             {5,6,7,8},
             {9,10,11,12},
             {13,14,15,0}};
+    // Piece 0 represents the space (empty) tile.
     private int spacePosX = 3;
     private int spacePosY = 3;
 
+    /**
+     * When printing the object, simply print the board array.
+     * @return The tiles in the board padded with 3 spaces and organised into rows and columns.
+     */
     @Override
     public String toString() {
         StringBuilder string = new StringBuilder();
@@ -24,14 +29,35 @@ public class PuzzleBoard {
         return string.toString();
     }
 
+
+    /**
+     * This simply returns the tile in the specific position supplied.
+     * @param x X-coord of the tile
+     * @param y Y-coord of the tile
+     * @return The tile number
+     */
     public String getTile(int x, int y) {
         return Integer.toString(board[y][x]);
     }
 
+
+    /**
+     * This use a helper method to handle generating random numbers from 0 to the max parameter.
+     *
+     * @param max The max value of the number to generate
+     * @return The random number
+     */
     private int randomRange(int max) {
         return (int) ((Math.random()) * max);
     }
 
+
+    /**
+     * This shuffles the board.
+     *
+     * @param moves How many moves to shuffle the board.
+     * @throws Exception When the tile to be moved is not a valid tile
+     */
     public void shuffleBoard(int moves) throws Exception {
         for (int i = 0; i < moves; i++) {
             ArrayList<Character> possibleMoves = possibleMoves();
@@ -45,6 +71,10 @@ public class PuzzleBoard {
         }
     }
 
+    /**
+     * This returns all the possible moves relative to the space tile.
+     * @return List of possible moves
+     */
     private ArrayList<Character> possibleMoves() {
         ArrayList<Character> possibleMoves = new ArrayList<>(Arrays.asList('u', 'd', 'l', 'r'));
         if (spacePosX == 3) {possibleMoves.remove((Character) 'r');}
@@ -54,23 +84,30 @@ public class PuzzleBoard {
         return possibleMoves;
     }
 
+    /**
+     * This will handle moves where multiple tiles are moved at once - a line of tiles over by one piece.
+     *
+     * @param posX X-coord of the tile
+     * @param posY Y-coord of the tile
+     * @throws Exception When the tile position is not valid
+     */
     private void longSlide(int posX, int posY) throws Exception {
-        if (spacePosX - posX == 0) {
+        if (spacePosX - posX == 0) { // If line of tiles is in the column direction
             if (spacePosY - posY > 0) { // to the left side
                 for (int i = 0; i < spacePosY - posY; i++) {
                     movePiece(spacePosX, spacePosY - 1);
                 }
-            } else {
+            } else { // to the right
                 for (int i = 0; i < posY - spacePosY; i++) {
                     movePiece(spacePosX, spacePosY + 1);
                 }
             }
-        } else {
+        } else { // If line of tiles is in the row direction
             if (spacePosX - posX > 0) { //above
                 for (int i = 0; i < spacePosX - posX; i++) {
                     movePiece(spacePosX - 1, spacePosY);
                 }
-            } else {
+            } else { // below
                 for (int i = 0; i < posX - spacePosX; i++) {
                     movePiece(spacePosX + 1, spacePosY);
                 }
@@ -79,6 +116,16 @@ public class PuzzleBoard {
         movePiece(posX, posY);
     }
 
+
+    /**
+     * This is the method called to move a piece.
+     * Contains the code required to move a piece by one space. If trying to move a line of pieces, it will call
+     * another method to handle.
+     * @param posX X-coord of the tile
+     * @param posY Y-coord of the tile
+     * @return // If (a) piece(s) has been moved.
+     * @throws Exception If the position supplied is invalid
+     */
     public boolean movePiece(int posX, int posY) throws Exception {
         if (posX > 3 || posY > 3) {
             throw new Exception("Position Invalid");
